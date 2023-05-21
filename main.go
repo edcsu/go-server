2358edbe1744e8bf184fe62aad74697fd30a3166
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-05-21 11:55:09 +0300

    chore: remove binary builds

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("Starting server at port 8000\n")
+	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
